09e1116319da17c385fb396f229c6bdd24fd1d51
mp0liiu <raian@reeshome.org>
2020-12-31 02:58:36 +0900

    react-router でコンポーネントの表示切替をしてみる

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const Home = () => {
+  return <h2>Home</h2>;
+}
+
+const About = () => {
+  return <h2>About</h2>;
+}
+
+const Users = () => {
+  return <h2>Users</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/about' exact>
+            <About />
+          </Route>
+          <Route path='/users' exact>
+            <Users />
+          </Route>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
