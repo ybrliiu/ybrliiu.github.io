2fa05f8af4cc6c2f31ef8df7d890584831eeb16e
mp0liiu <raian@reeshome.org>
2021-01-01 14:44:33 +0900

    文字を揺らした

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,11 +1,14 @@
-import style from "./home.module.css";
+import style from "./home.module.scss";
 import icon from "../assets/images/icon.png";
 
 export function Home() {
+  const name = 'LIIU';
   return (
     <div className={style.container}>
       <img className={ style.icon } src={ icon } alt="icon" />
-      <div className={ style.name }>YBRLIIU</div>
+      <div className={ style.name } data-text={ name }>
+        { name }
+      </div>
     </div>
   );
 }
