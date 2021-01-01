0d2b396a79e32cc8da81bc81325875b5865859f8
mp0liiu <raian@reeshome.org>
2021-01-01 19:19:20 +0900

    デザイン修正

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -7,7 +7,7 @@ export function AboutMe() {
       <section className={ cardStyle['card-container'] }>
         <h1 className={ cardStyle.title }>ABOUT ME</h1>
         <p className={ cardStyle.contents }>
-          インターネットでは id: ybrliiu / mp0liiu という名前で活動していることが多いです。<br />
+          インターネットでは ybrliiu / mp0liiu というidで活動していることが多いです。<br />
           東京のIT系の会社でソフトウェアエンジニアをしています。<br />
           仕事ではWebアプリケーションのサーバーサイドをいじっていることが多いです。<br />
           好きな言語は Perl と TypeScript です。<br />
